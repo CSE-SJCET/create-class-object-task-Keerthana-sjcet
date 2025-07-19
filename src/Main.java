1a49ab1b686980d826110e7d445bc3a299d146a6
Keerthana G Pillai <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-07-19 18:10:41 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
-     // TODO: Create a class called Student with name, rollNumber, and grade
-
-    // TODO: Create a method to display student details
-public class Main {
+package demo;
+class Student{
+	
+	public String name;
+	public int rollNumber;
+	public String grade;
+	
+	Student(){
+		name="Ram";
+		rollNumber=47;
+		grade="S";
+	}
+	public void displayDetails(){
+		System.out.println("Name: "+name);
+		System.out.println("Rollno: "+rollNumber);
+		System.out.println("Grade: "+grade);
+	}
+	
+	// TODO: Create a method to display student details
+}
+public class OOPSDEMO {
       public static void main(String[] args) {
+    	  Student std=new Student();
+    	  std.displayDetails();
         // TODO: Create a Student object and call the method to display details
     }
 }
